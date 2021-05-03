4d4244d186e081ac262159e1f293d19265703cf7
tarrendt <tarrendt@devo.reclaimhosting.com>
2021-05-03 18:38:20 -0400

    tp5

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
 window.onload = function() {
-  x = document.getElementById("title_0");
-  x.classList.add("header_s");
+ document.getElementById('header_s').style.fontSize = '50px'
 }
- 
 
+window.onclick = function () {
+   document.getElementById('header_s').style.color = 'lightblue'
+   }
 
+list = document.querySelectorAll("h4");
+  console.log(list);
+  list.item(0).addEventListener("click", function() {
+    document.querySelector('#ingredients_list').classList.toggle('hide');
+  });
 
+  list.item(1).addEventListener("click", function() {
+    document.querySelector('#equipment_list').classList.toggle('hide');
+  });
 
-
-
-
-
-//function my_change() {
-//x = document.getElementById("header");
-//x.classList.add("header_s"); 
-//}
- 
-//window.onload = my_change();
+  list.item(2).addEventListener("click", function() {
+    document.querySelector('#highlight').classList.toggle('hide');
+  });
+};
