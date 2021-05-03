5163384c46caf79e681ebd6bd9398640a0ee93a2
tarrendt <tarrendt@devo.reclaimhosting.com>
2021-05-03 16:35:21 -0400

    up tp5

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-function my_change() {
-x = document.getElementById("header");
-x.classList.add("header_s"); 
+window.onload = function() {
+  x = document.getElementById("title_0");
+  x.classList.add("header_s");
 }
  
-window.onload = my_change();
+
+
+
+
+
+
+
+
+//function my_change() {
+//x = document.getElementById("header");
+//x.classList.add("header_s"); 
+//}
+ 
+//window.onload = my_change();
